d64b067334098a92d861d4f14f76277c27a3d31d
Oleksii Nikolaiev <oleksii.nikolaiev.on@gmail.com>
2024-04-05 21:52:31 +0300

    Refactored AboutBgr

--- src/components/AboutBgr/AboutBgr.jsx
+++ src/components/AboutBgr/AboutBgr.jsx
@@ -1,18 +1,21 @@
-import { getImageObjects } from "./helperAboutBgr";
+// import { getImageObjects } from "./helperAboutBgr";
 import * as s from "./AboutBgr.styled";
 
 const AboutBgr = ({ children }) => {
-  const imageObjects = getImageObjects();
+  // const imageObjects = getImageObjects();
 
   return (
     <s.BgrWrap
-      src={imageObjects.mobile["@1x"].png}
-      srcMobile1x={imageObjects.mobile["@1x"].png}
-      srcMobile2x={imageObjects.mobile["@2x"].png}
-      srcTablet1x={imageObjects.tablet["@1x"].png}
-      srcTablet2x={imageObjects.tablet["@2x"].png}
-      srcDesktop1x={imageObjects.desktop["@1x"].png}
-      srcDesktop2x={imageObjects.desktop["@2x"].png}
+      // src={imageObjects.mobile["@1x"].png}
+      // srcMobile1x={imageObjects.mobile["@1x"].png}
+      // srcMobile2x={imageObjects.mobile["@2x"].png}
+      // srcTablet1x={imageObjects.tablet["@1x"].png}
+      // srcTablet2x={imageObjects.tablet["@2x"].png}
+      // srcDesktop1x={imageObjects.desktop["@1x"].png}
+      // srcDesktop2x={imageObjects.desktop["@2x"].png}
+      style={{
+        backgroundImage: `url("/src/assets/images/about-img/ape-about-desktop@1x.webp"`,
+      }}
     >
       {children}
     </s.BgrWrap>
